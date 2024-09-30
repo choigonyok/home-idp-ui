f26268d36fbe087226ff2684ae916571a93616dd
achoistic98 <achoistic98@naver.com>
2024-09-30 14:09:29 +0900

    feat: request manifest to backend

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+git submodule update --remote              
+
 # home-idp-ui
 frontend for home-idp open source software
 
@@ -73,3 +75,47 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+9/19 졸프미팅1
+플랫폼 엔지니어링
+
+기능적인 부분들 정의, 데브옵스 정의
+데브옵스의 특징 중 해결되지 않은 이슈들 버그, 복잡도 등 분석 및 정리
+
+그래서 이런 이슈들로 인해 플래폼 엔지니어링이 대두된 상황 설명
+플랫폼 엔지니어링에서 데브옵스의 
+
+논문 양식대로 작성해야함
+개발/데모보다는 문제분석과 어떤 알고리즘과 어떤 방식으로 어떻게 데브옵스의 문제를 해결하였는지를 논문으로 증명해야함
+
+주제 ; 뭐를 위한 플랫폼 엔지니어링인지, 특징(대상, 가볍다, 효율적이다 등)
+도메인이나 문제점을 해결하기 위한 
+
+목요일 5시반마다 매주 졸업프로젝트 미팅
+
+9/27 졸프미팅2
+
+데브옵스의 복잡성을 해결하기 위한 도구로 플랫폼 엔지니어링이 대두되고 사용되는데, 이를 어떻게 논문으로 수치화해서 증명해야하는 것인지?
+데브옵스의 인지부하를 최소화하고 개발자들이 로직에 집중할 수 있도록 해주는 플랫폼 엔지니어링
+
+backstage라는 IDP 구축 오픈소스 소프트웨어가 존재하는데, 
+backstage를 사용하기 위해서는 다양한 YAML 형식의 파일들을 통해서 데이터를 주입해야하고, 데이터 모델들이 지정되어있다는 것이다
+
+2. 지정된 데이터 모델
+Component
+Resource
+API
+User
+Group
+System, and 
+Domain
+
+
+
+특정 환경으로 고정
+
+클라우드 네이티브 어플리케이션을 위한 IDP 엔지니어링
+쿠버네티스 기반으로 개발할 때 필요한 것을 리스트업
+자동화 되지 않고 사람이 하는 것들을 리스트업해서 어떤 부분을 특화해서 해결할지
+쿠버네티스의 오케스트레이션 기술에서 제공되지 않는 부분들을 조사
--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleSubmit = async () => {
+    const schema = process.env.HOME_IDP_BACKEND_SCHEMA;
+    const host = process.env.HOME_IDP_BACKEND_HOST;
+    const port = process.env.HOME_IDP_BACKEND_PORT;
+    
+    const url = `${schema}://${host}:${port}/manifest?username=choigonyok`;s
+
+    const bodyData = {
+      message: '@@@@@@@@@@@@@@@@This is the body content@@@@@@@@@@@@@@@@@@@@@@@@@@',
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyData),
+      });
+
+      const data = await response.json();
+      setResponseMessage(data.message);
+    } catch (error) {
+      console.error('Error sending request:', error);
+      setResponseMessage('Error sending request');
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Send POST request to test.com</h1>
+      <button onClick={handleSubmit}>Send Request</button>
+      <p>Response: {responseMessage}</p>
     </div>
   );
 }
-
 export default App;
